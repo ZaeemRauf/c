27ef31a1ac9f1936f506700c9f407b1b27da5f0f
Dawood Akram <dawoodakram33@gmail.com>
2017-10-24 14:50:45 +0500

    Working Code

--- Average age.cpp
+++ Average age.cpp
@@ -1,27 +1,28 @@
-
-#include <iostream.h>
+#include <iostream>
+using namespace std;
 main()
 {
  int age1,age2,age3,age4,age5,age6,age7,age8;
  int TotalAge;
  int AverageAge;
-  Cout <<"please enter the age of student 1:";
- Cin >> age1;
-  Cout <<"please enter the age of student 2:";
- Cin >> age2;
-  Cout <<"please enter the age of student 3:";
- Cin >> age3;
-  Cout <<"please enter the age of student 4:";
- Cin >> age4;
-  Cout <<"please enter the age of student 5:";
- Cin >> age5;
-  Cout <<"please enter the age of student 6:";
- Cin >> age6;
-  Cout <<"please enter the age of student 7:";
- Cin >> age7;
-  Cout <<"please enter the age of student 8:";
- Cin >> age8;
+  cout <<"please enter the age of student 1:";
+ cin >> age1;
+  cout <<"please enter the age of student 2:";
+ cin >> age2;
+  cout <<"please enter the age of student 3:";
+ cin >> age3;
+  cout <<"please enter the age of student 4:";
+ cin >> age4;
+  cout <<"please enter the age of student 5:";
+ cin >> age5;
+  cout <<"please enter the age of student 6:";
+ cin >> age6;
+  cout <<"please enter the age of student 7:";
+ cin >> age7;
+  cout <<"please enter the age of student 8:";
+ cin >> age8;
  TotalAge = age1+age2+age3+age4+age5+age6+age7+age8;
- AverageAge= TotalAge/8
- Cout<<"The average age of the class is:"<<AverageAge;
+ AverageAge= TotalAge/8;
+ cout <<"The average age of the class is:"
+ <<AverageAge;
 }
