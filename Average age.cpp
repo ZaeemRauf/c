43bc5e692f2ae671b1dec7fc42d28dfedbd0d269
ZaeemRauf <33054737+ZaeemRauf@users.noreply.github.com>
2017-10-24 02:09:02 -0700

    initial commit

---
+++ Average age.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream.h>
+main()
+{
+ int age1,age2,age3,age4,age5,age6,age7,age8;
+ int TotalAge;
+ int AverageAge;
+  Cout <<"please enter the age of student 1:";
+ Cin >> age1;
+  Cout <<"please enter the age of student 2:";
+ Cin >> age2;
+  Cout <<"please enter the age of student 3:";
+ Cin >> age3;
+  Cout <<"please enter the age of student 4:";
+ Cin >> age4;
+  Cout <<"please enter the age of student 5:";
+ Cin >> age5;
+  Cout <<"please enter the age of student 6:";
+ Cin >> age6;
+  Cout <<"please enter the age of student 7:";
+ Cin >> age7;
+  Cout <<"please enter the age of student 8:";
+ Cin >> age8;
+ TotalAge = age1+age2+age3+age4+age5+age6+age7+age8;
+ AverageAge= TotalAge/8
+ Cout<<"The average age of the class is:"<<AverageAge;
+}
